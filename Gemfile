04146f66d9ed7da965da9cb2da04223771b2c6dc
zeke <zeke@sikelianos.com>
2012-06-17 22:04:22 -0700

    fresh rails app

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+gem 'pg'
+gem 'haml-rails'
+gem 'compass'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Queriac::Application.config.secret_token = '7fcb1bd3f63c33b2ad2ee02e225d547d1943ab3c73020a7a2531d8270a34b5541f12e8ef4d983487070bcc307efdec32eebdb345b4c8fcb08db22c6e670a3956'
