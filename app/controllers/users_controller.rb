e7f121ef170a94ef0b726b066096a59a4cb78134
zeke <zeke@sikelianos.com>
2012-08-08 14:06:41 -0700

    sort commands by freshness

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_nickname(params[:id])
-    @commands = @user.commands.order("keyword ASC").all
+    @commands = @user.commands.order("commands.updated_at DESC").all
   end
   
 end
