17252e5b5db359d0a84ec269ebd2cb9d680ce004
zeke <zeke@sikelianos.com>
2012-08-11 01:22:31 -0700

    Rails 3.1+ has Rack SSL enforcement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :ensure_ssl
-  
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
@@ -21,10 +19,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  
-  def ensure_ssl
-    redirect_to :protocol => "https://" unless request.ssl? || request.local?
   end
   
 end
