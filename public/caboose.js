e8c72eb3e1706df150a5671b0138b1278378d6ac
zeke <zeke@sikelianos.com>
2012-08-20 00:03:31 -0700

    Include engine version number as a query param

--- public/caboose.js
+++ public/caboose.js
@@ -2,7 +2,7 @@ Zepto(
   function($) {
     _s=document.createElement('script');
     _s.type='text/javascript';
-    _s.src='https://queriac2.herokuapp.com/exec/%s';
+    _s.src='https://queriac2.herokuapp.com/exec/%s?v=1.0';
     document.getElementsByTagName('head')[0].appendChild(_s);
     void(0);
   }
